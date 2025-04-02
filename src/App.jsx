aa980f9929740d17b3fc22dbf01b7597f520fc39
KUSHALMANDALA07 <kushalmandala123@gmail.com>
2025-04-02 14:26:48 +0530

    assignment-2

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Name: Kushal</h1>
+     <h3>email Id: mandala.kushal.s89@kalvium.community</h3>
+    </>
+  )
+}
+
+export default App
